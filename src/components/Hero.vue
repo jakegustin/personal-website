6a4ff5b5a0a65ba75cd8839fb2467ad8408050a3
Jake Gustin <jakegust2020@gmail.com>
2024-11-23 20:27:42 -0500

    remove email, should move to contact page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -13,7 +13,6 @@
                 <h2 class="text-4xl font-sans font-medium">Software Developer / Systems Engineer</h2>
             </div>
             <p class="text-xl">I am a recent graduate of Boston University's Dual Degree program, majoring in Computer Science along with Film & Television. I've leveraged my unique skillset to develop a variety of high quality software products, from high-quality user interfaces to efficient, low-level systems code. I am currently seeking admission into a Master's of Computer Science program for the upcoming 2025-2026 academic year.</p>
-            <p class="text-xl">Email: jakegust2020@gmail.com</p>
             <div class="w-100" id="infoResumeButton">
                 <button class="w-60 h-20 bg-gray-800 text-white text-xl rounded-2xl font-sans hover:scale-110 transition-transform duration-100 shadow-xl">See My Resume</button> 
             </div>
