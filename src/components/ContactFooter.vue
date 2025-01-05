9dc4ae4648a7416f547528f2a4190082c11ead00
Jake Gustin <jakegust2020@gmail.com>
2025-01-05 14:00:32 -0500

    add copyright to footer

--- src/components/ContactFooter.vue
+++ src/components/ContactFooter.vue
@@ -1,8 +1,9 @@
 <template>
-<div class="flex flex-col justify-center items-center bg-blue-800 dark:bg-blue-900 transition-colors duration-300 ease-in-out gap-y-10 py-10">
+<div class="flex flex-col justify-center items-center bg-blue-800 dark:bg-blue-900 transition-colors duration-300 ease-in-out gap-y-5 pt-8 pb-3">
     <h3 class="text-3xl font-bold text-white">Want to learn more?</h3>
     <RouterLink to="/contact">
         <button class="w-60 h-20 bg-gray-800 text-white text-xl rounded-2xl hover:scale-110 transition-transform duration-100 shadow-xl">Reach Out!</button>
     </RouterLink>
+    <p class="text-xs">&copy 2025 Jake Gustin. All Rights Reserved.</p>
 </div>
 </template>
